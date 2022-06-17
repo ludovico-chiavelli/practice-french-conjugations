066d4a0026d32a4ed4cf1f8fe797d443a8165c3a
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-06-17 11:48:20 +0100

    finished styling

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-import { Navbar } from "./"
+import { Navbar, Form } from "./"
 
 export const Container = () => {
     return(
         <div className="h-full" style={{backgroundColor: '#283D3B'}}>
             <Navbar/>
             <div className="h-5/6 flex justify-center">
-                <div className="w-3/4  max-w-2xl mt-10 rounded-md" style={{backgroundColor: '#EDDDD4'}}>
-                    hello
+                <div className="w-3/4  max-w-2xl mt-10 rounded-md flex justify-center items-center" style={{backgroundColor: '#EDDDD4'}}>
+                    <Form/>
                 </div>
             </div>
         </div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,9 +2,9 @@ import React from "react"
 
 export const Form = () => {
     return(
-        <form className="w-3/5 flex flex-col">
+        <form className="h-3/5 w-3/5 flex flex-col justify-center items-center">
             <label for="verb">(verb)</label>
-            <input type="text" id="verb" name="verb" className="rounded-t-md bg-black opacity-50"></input>
+            <input type="text" id="verb" name="verb" className="rounded-t-md bg-black/10 border-b-2 mt-10" style={{borderColor: '#DB786C'}}></input>
         </form>
     )
 }

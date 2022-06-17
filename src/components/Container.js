dfb95ea8d1c2e449b918f7bcab93a41a638c1cf4
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-06-17 10:43:13 +0100

    created container component

---
+++ src/components/Container.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Container = () => {
+    return(
+        <div className="h-screen flex justify-center">
+            <div className="h-full w-8/12 mt-10 rounded-md" style={{backgroundColor: '#EDDDD4'}}>
+                hello
+            </div>
+        </div>
+    )
+}

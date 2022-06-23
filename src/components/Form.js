857aa7372d7bc72d061e3b255068e4b8e9a6943b
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-06-23 10:33:15 +0100

    changed input height

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,7 @@ export const Form = () => {
     return(
         <form className="h-3/5 w-3/5 flex flex-col justify-center items-center">
             <label for="verb">(verb)</label>
-            <input type="text" id="verb" name="verb" className="rounded-t-md bg-black/10 border-b-2 mt-10" style={{borderColor: '#DB786C'}}></input>
+            <input type="text" id="verb" name="verb" className="h-10 rounded-t-md bg-black/10 border-b-2 mt-10" style={{borderColor: '#DB786C'}}></input>
         </form>
     )
 }

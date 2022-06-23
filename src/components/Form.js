6a6c6e4867c8d6a502050fdabb6003b3866a04ad
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-06-23 11:23:57 +0100

    made form elements a bit bigger

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,8 +3,8 @@ import React from "react"
 export const Form = () => {
     return(
         <form className="h-3/5 w-3/5 flex flex-col justify-center items-center">
-            <label for="verb">(verb)</label>
-            <input type="text" id="verb" name="verb" className="h-10 rounded-t-md bg-black/10 border-b-2 mt-10" style={{borderColor: '#DB786C'}}></input>
+            <label for="verb" className="text-3xl">(verb)</label>
+            <input type="text" id="verb" name="verb" className="md:h-10 md:w-72 md:scale-150 rounded-t-md bg-black/10 border-b-2 mt-10" style={{borderColor: '#DB786C'}}></input>
         </form>
     )
 }

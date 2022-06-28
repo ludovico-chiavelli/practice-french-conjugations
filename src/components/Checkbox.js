82826b739347ebb3db0185561b23a708bb3a305b
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-06-28 15:02:11 +0100

    fix double rendering bug

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -4,7 +4,7 @@ export const Checkbox = ({ checked, tense, onChange }) => {
     return(
         <div className="w-full px-2 py-1 hover:bg-[#197278]/50" onClick={() => onChange(tense)} key={tense}>
             <input type="checkbox" id={tense} checked={checked} readOnly/>
-            <label htmlFor={tense} className="ml-2">{tense}</label>
+            <label htmlFor={tense} className="ml-2" onClick={(e) => e.preventDefault()}>{tense}</label>
         </div>
     )
 }

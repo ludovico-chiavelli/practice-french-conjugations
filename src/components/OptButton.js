e8b779dd17698c1627335234dfa0617225c5ac53
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-06-28 08:17:22 +0100

    hide text overflow

--- src/components/OptButton.js
+++ src/components/OptButton.js
@@ -4,7 +4,7 @@ export const OptButton = ({ title, children }) => {
     return(
         <button className="h-20 w-full ring-2 ring-[#197278]/30 rounded-md px-4 py-2">
             <h3 className="w-full text-left text-[#197278] text-base font-bold">{title}</h3>
-            <p className="w-full text-left">{children}</p>
+            <p className="w-full text-left overflow-hidden text-ellipsis">{children}</p>
         </button>
     )
 }

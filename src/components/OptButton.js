e38fcb93b543175897348ff13f77c18845c80322
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-06-28 11:16:46 +0100

    add menu displaying functionality

--- src/components/OptButton.js
+++ src/components/OptButton.js
@@ -1,10 +1,24 @@
-import React from "react"
+import React, { useState } from "react"
 
-export const OptButton = ({ title, children }) => {
+import { Checkbox } from "./Checkbox";
+
+export const OptButton = ({ tenses, title, children }) => {
+    const [show, setShow] = useState(false);
+
     return(
-        <button className="h-20 w-full ring-2 ring-[#197278]/30 rounded-md px-4 py-2">
-            <h3 className="w-full text-left text-[#197278] text-base font-bold">{title}</h3>
-            <p className="w-full text-left overflow-hidden text-ellipsis">{children}</p>
-        </button>
+        <div>
+            <button className="h-20 w-full ring-2 ring-[#197278]/30 rounded-md px-4 py-2" onClick={() => setShow(!show)}>
+                <h3 className="w-full text-left text-[#197278] text-base font-bold">{title}</h3>
+                <p className="w-full text-left overflow-hidden text-ellipsis">{children}</p>
+            </button>
+            {
+                show && 
+                <div className="fixed inset-0 bg-black bg-opacity-50" onClick={() => setShow(!show)}>
+                    <div className="fixed h-[33vh] inset-x-0 bottom-0 rounded-t-md bg-[#EDDDD4]" onClick={(e) => {e.stopPropagation();}}>
+                        <Checkbox tenses={tenses}/>
+                    </div>
+                </div>
+            }
+        </div>
     )
 }

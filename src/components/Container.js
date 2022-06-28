958340eee8b0a7e79fb410c49391be612f137eb2
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-06-28 09:58:31 +0100

    tailwind defines bg color for container

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-import { Navbar, Form, Options } from "./"
+import { Navbar, Options } from "./"
 
 export const Container = () => {
     return(
-        <div className="h-full" style={{backgroundColor: '#283D3B'}}>
+        <div className="h-full bg-[#283D3B]">
             <Navbar/>
             <div className="h-5/6 flex justify-center">
                 <div className="w-3/4  max-w-2xl mt-10 rounded-md flex justify-center items-center bg-[#EDDDD4]">

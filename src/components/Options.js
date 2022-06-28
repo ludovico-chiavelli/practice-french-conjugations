54618b87541314d6ca4233cd663b0b93aa1bbedc
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-06-28 11:17:18 +0100

    assign tense state handling to Options.js

--- src/components/Options.js
+++ src/components/Options.js
@@ -3,10 +3,12 @@ import React from "react";
 import { OptButton } from "./OptButton";
 
 export const Options = () => {
+    const tenses = ["present", "past participle"]
+
     return(
         <section className="w-4/5 md:max-w-4xl">
             <h1>Conjugation Drill</h1>
-            <OptButton title="Tenses">
+            <OptButton title="Tenses" tenses={tenses}>
                 something
             </OptButton>
         </section>

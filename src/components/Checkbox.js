346580116f778e76027f18a877e934d4328199ba
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-06-28 11:22:22 +0100

    OptButton handle list rendering

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,15 +1,12 @@
 import React, { useState } from "react"
 
-export const Checkbox = ({ tenses }) => {
+export const Checkbox = ({ tense }) => {
     const [checked, setChecked] = useState(false);
-    const tensesItems = tenses.map((tense) => 
-        <div className="w-full px-2 py-1 hover:bg-[#197278]/50" onClick={() => setChecked(!checked)} key={tense}>
-            <input type="checkbox" id={tense} defaultChecked={checked}/>
-            <label htmlFor={tense} className="ml-2">{tense}</label>
-        </div>
-    )
 
     return(
-        tensesItems
+        <div className="w-full px-2 py-1 hover:bg-[#197278]/50" onClick={() => setChecked(!checked)} key={tense}>
+            <input type="checkbox" id={tense} checked={checked}/>
+            <label htmlFor={tense} className="ml-2">{tense}</label>
+        </div>
     )
 }
--- src/components/OptButton.js
+++ src/components/OptButton.js
@@ -5,6 +5,10 @@ import { Checkbox } from "./Checkbox";
 export const OptButton = ({ tenses, title, children }) => {
     const [show, setShow] = useState(false);
 
+    const tensesItems = tenses.map((tense) => 
+        <Checkbox tense={tense}/>
+    )
+
     return(
         <div>
             <button className="h-20 w-full ring-2 ring-[#197278]/30 rounded-md px-4 py-2" onClick={() => setShow(!show)}>
@@ -14,8 +18,8 @@ export const OptButton = ({ tenses, title, children }) => {
             {
                 show && 
                 <div className="fixed inset-0 bg-black bg-opacity-50" onClick={() => setShow(!show)}>
-                    <div className="fixed h-[33vh] inset-x-0 bottom-0 rounded-t-md bg-[#EDDDD4]" onClick={(e) => {e.stopPropagation();}}>
-                        <Checkbox tenses={tenses}/>
+                    <div className="fixed h-52 inset-x-0 bottom-0 rounded-t-md bg-[#EDDDD4]" onClick={(e) => {e.stopPropagation();}}>
+                        {tensesItems}
                     </div>
                 </div>
             }

40e55f32a725f4d3f3f05a0147308a513cafb495
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-06-29 11:45:32 +0100

    set min width

--- src/components/Container.js
+++ src/components/Container.js
@@ -4,7 +4,7 @@ import { Navbar, Options } from "./"
 
 export const Container = () => {
     return(
-        <div className="h-full bg-[#283D3B]">
+        <div className="h-full min-w-[400px] bg-[#283D3B]">
             <Navbar/>
             <div className="h-5/6 flex justify-center">
                 <div className="w-3/4  max-w-2xl mt-10 rounded-md flex justify-center items-center bg-[#EDDDD4]">

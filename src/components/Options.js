5a8ab3e5a44115865dfff7f5a57bdb0f9d5c81ad
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-06-29 11:25:39 +0100

    add pronouns button

--- src/components/Options.js
+++ src/components/Options.js
@@ -21,11 +21,21 @@ export const Options = () => {
         "Stem-changing and Irregular",
     ]
 
+    const pronouns = [
+        "Je",
+        "Tu",
+        "Il/Elle/On",
+        "Nous",
+        "Vous",
+        "Ils/Elles",
+    ]
+
     return(
         <section className="w-4/5 md:max-w-4xl">
             <h1>Conjugation Drill</h1>
             <OptButton title="Tenses" options={tenses}/>
             <OptButton title="Categories" options={categories}/>
+            <OptButton title="Pronouns" options={pronouns}/>
         </section>
     )
 }

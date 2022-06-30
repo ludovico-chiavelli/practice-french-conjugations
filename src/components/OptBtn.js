91c6068ebf2062ae551228d72c7ff26a2c6a05fa
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-06-30 10:00:17 +0100

    make button content justify-around

--- src/components/OptBtn.js
+++ src/components/OptBtn.js
@@ -52,7 +52,7 @@ export const OptBtn = ({ options, blurb, children, btnType }) => {
 
     return(
         <div className="sm:relative first:mt-4 my-4">
-            <button className="h-16 w-full ring-2 ring-[#197278]/30 rounded-md px-4 py-2 flex flex-col justify-between" onClick={() => setShow(!show)}>
+            <button className="h-16 w-full ring-2 ring-[#197278]/30 rounded-md px-4 py-2 flex flex-col justify-around" onClick={() => setShow(!show)}>
                 {children}
                 {btnType === "selection" ? blurb(checks) : blurb()}
             </button>

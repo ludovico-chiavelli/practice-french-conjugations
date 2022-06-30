fcc5829bc00908c329ca89cf7b00680117753580
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-06-30 08:19:24 +0100

    add verbs button

--- src/components/Options.js
+++ src/components/Options.js
@@ -30,12 +30,23 @@ export const Options = () => {
         "Ils/Elles",
     ]
 
+    const selectionOfVerbs = [
+        "All verbs",
+        "Most common verbs"
+    ]
+
     return(
         <section className="w-4/5 md:max-w-4xl">
-            <h1>Conjugation Drill</h1>
-            <OptButton title="Tenses" options={tenses}/>
-            <OptButton title="Categories" options={categories}/>
-            <OptButton title="Pronouns" options={pronouns}/>
+            <div>
+                <h1>Conjugation Drill</h1>
+                <OptButton title="Tenses" options={tenses}/>
+                <OptButton title="Categories" options={categories}/>
+                <OptButton title="Pronouns" options={pronouns}/>
+            </div>
+            <div className="w-32 mt-10">
+                <h3>Verbs</h3>
+                <OptButton title="All Verbs" options={selectionOfVerbs} hasBlurb={false}/>
+            </div>
         </section>
     )
 }

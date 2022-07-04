22a3035942d5852de763667cf229f345067ebfe5
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-07-04 11:10:53 +0100

    render blurb

--- src/components/BtnContent.js
+++ src/components/BtnContent.js
@@ -1,5 +1,17 @@
 import React from "react";
 
-export const BtnContent = () => {
-    
+export const BtnContent = ({ checks }) => {
+    let selected = ""
+    Object.keys(checks).forEach(item => {
+        if (checks[item]) {
+            selected = selected + item + " "
+        }
+    })
+
+    return(
+        <p className="max-w-full text-left overflow-hidden whitespace-nowrap text-ellipsis text-black/40">
+            {selected}
+        </p>
+    )
+
 }

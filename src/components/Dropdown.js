1981ee5d5b297a9d7111eb9be29df087c278481d
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-07-04 10:23:10 +0100

    remove unnecessary onClick handling

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -17,7 +17,7 @@ export const Dropdown = ({ show }) => {
     const chooseMenu = () => {
         if (show && isDesktop) {
             return(
-                <div className="absolute h-40 w-full z-10 mt-2 rounded-md bg-[#EDDDD4] drop-shadow-lg overflow-scroll ring-1" onClick={(e) => {e.stopPropagation();}}>
+                <div className="absolute h-40 w-full z-10 mt-2 rounded-md bg-[#EDDDD4] drop-shadow-lg overflow-scroll ring-1">
                     {items}
                 </div>
             ) 

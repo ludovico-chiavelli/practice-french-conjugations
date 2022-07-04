0203261fec12107ebb6e6926cb0b4e71d56e6b6f
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-07-04 11:47:42 +0100

    capitalize component name

--- src/components/verbsBtn.js
+++ src/components/verbsBtn.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-export const verbsBtn = ({ options }) => {
+export const VerbsBtn = ({ options }) => {
     const [sValue, setSValue] = useState(options[0])
 
     const items = options.map((option) => <option value={option}>{option}</option>)

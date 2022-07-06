7bd293d51c4ec8cb49c1f7c8703e8f538f38b2f2
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-07-06 11:16:07 +0100

    add Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css'
 
+import { BrowserRouter as Router} from "react-router-dom";
+
 import { Container } from "./components"
 
 export default function App() {
   return (
-    <div className='App'>
-      <Container/>
-    </div>
+    <Router>
+      <div className='App'>
+        <Container/>
+      </div>
+    </Router>
   )
 }

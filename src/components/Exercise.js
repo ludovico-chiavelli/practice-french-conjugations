ae96912e6c427a0941a7deeadb183dc81073591f
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-07-07 10:42:41 +0100

    add styling

--- src/components/Exercise.js
+++ src/components/Exercise.js
@@ -1,5 +1,12 @@
 import React from "react";
 
 export const Exercise = () => {
-  
+  return(
+    <section className="h-full w-4/5 md:max-w-4xl flex flex-col justify-center">
+      <div className="flex justify-center">
+        <p>tu</p>
+        <input type="text"/>
+      </div>
+    </section>
+  )
 }

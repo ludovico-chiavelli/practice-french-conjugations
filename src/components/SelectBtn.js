93cd1cd5ecd911ed8d37a25f535f2502f4c65021
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-07-11 15:32:36 +0100

    add updating to SelectBtns

--- src/components/SelectBtn.js
+++ src/components/SelectBtn.js
@@ -3,7 +3,11 @@ import React, { useState } from "react";
 export const SelectBtn = ({ options }) => {
     const [sValue, setSValue] = useState(options[0])
 
-    const items = options.map((option) => <option value={option}>{option}</option>)
+    const handleClick = (option) => {
+        setSValue(option)
+    }
+
+    const items = options.map((option) => <option value={option} onClick={() => handleClick(option)}>{option}</option>)
 
     return(
         <select value={sValue} className="h-12 w-56 ring-2 ring-[#197278]/30 rounded-md px-4 py-2 bg-inherit">

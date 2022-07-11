1fc2db71c3db5226f4dad50f96c9677ee2df9c6a
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-07-11 15:16:50 +0100

    make Checkbox observer

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,10 +1,17 @@
-import React from "react"
+import { observable } from "mobx"
+import React, { useContext } from "react"
 
-export const Checkbox = ({ checked, item, onOptionChange }) => {
+import { observer } from "mobx-react-lite"
+
+import { OptionsContext } from "../App"
+
+export const Checkbox = observer(({ type, checked, item }) => {
+    const drillOptions = useContext(OptionsContext)
+
     return(
-        <div className="w-full px-2 py-1 hover:bg-[#197278]/50" onClick={() => onOptionChange(item)} key={item}>
+        <div className="w-full px-2 py-1 hover:bg-[#197278]/50" onClick={() => drillOptions.selectOptions(type, item)} key={item}>
             <input type="checkbox" id={item} checked={checked} readOnly/>
             <label htmlFor={item} className="ml-2" onClick={(e) => e.preventDefault()}>{item}</label>
         </div>
     )
-}
+})

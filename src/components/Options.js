359a7ed687d72905258c8e3f309a76b4b9940f67
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-07-11 13:25:48 +0100

    set options using drillOptions object

--- src/components/Options.js
+++ src/components/Options.js
@@ -4,23 +4,25 @@ import { Link } from "react-router-dom";
 
 import { PreferencesBtn, SelectBtn } from ".";
 
+import drillOptions from "../stores/optionsStore";
+
 export const Options = () => {
 
     return(
         <section className="h-full w-4/5 md:max-w-4xl">
             <div>
                 <h1 className="my-10 text-4xl text-center">Conjugation Drill</h1>
-                <PreferencesBtn title="Tenses" options={tenses}/>
-                <PreferencesBtn title="Categories" options={categories}/>
-                <PreferencesBtn title="Pronouns" options={pronouns}/>
+                <PreferencesBtn title="Tenses" options={Object.keys(drillOptions.tenses)}/>
+                <PreferencesBtn title="Categories" options={Object.keys(drillOptions.categories)}/>
+                <PreferencesBtn title="Pronouns" options={Object.keys(drillOptions.pronouns)}/>
             </div>
             <div className="mt-10">
                 <h3>Verbs</h3>
-                <SelectBtn options={selectionOfVerbs}/>
+                <SelectBtn options={Object.keys(drillOptions.selectionOfVerbs)}/>
             </div>
             <div className="my-10">
                 <h3>Length</h3>
-                <SelectBtn options={numWords}/>
+                <SelectBtn options={Object.keys(drillOptions.numWords)}/>
             </div>
             <Link to="/exercise">Start drill</Link>
         </section>

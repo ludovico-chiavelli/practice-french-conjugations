587a77ea6f91aaa5d49066580a9aecc57162c1f5
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-07-13 11:04:21 +0100

    import checkAnswer util and delete redundant code

--- src/components/DisplayExercise.js
+++ src/components/DisplayExercise.js
@@ -1,16 +1,14 @@
 import React, { useState } from "react";
 
+import checkAnswer from "../utils/checkAnswer";
+
 export const DisplayExercise = ({exercises}) => {
     const [value, setValue] = useState('')
-    const [currExec, setCurrExec] = useState(exercises[0])
     const [execIndex, setExecIndex] = useState(0)
 
     const handleKeyPress = (e) => {
         if(e.key === "Enter"){
-            console.log(currExec)
-            console.log(execIndex)
             execIndex < exercises.length - 1 ? setExecIndex(execIndex + 1) : setExecIndex(0)
-            setCurrExec(exercises[execIndex])
             setValue('')
         }
       }
@@ -22,16 +20,16 @@ export const DisplayExercise = ({exercises}) => {
     return(
         <div className="flex justify-center">
             <div className="grow flex flex-col justify-end">
-                <span className="pr-2 text-end">{currExec.pronoun}</span>
+                <span className="pr-2 text-end">{exercises[execIndex].pronoun}</span>
             </div>
             <div className="grow-0 text-center">
                 <div>
-                    {currExec.tense}
+                    {exercises[execIndex].tense}
                 </div>
                 <input value={value} onChange={handleChange} type="text" onKeyDown={handleKeyPress}/>
             </div>
             <div className="grow flex flex-col justify-end">
-                <span className="pl-2 text-start">({currExec.verb})</span>
+                <span className="pl-2 text-start">({exercises[execIndex].verb})</span>
             </div>
         </div>
     )

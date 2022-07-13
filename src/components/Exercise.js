9ef738c5c03ad623a966e892075da55e2ab3b30c
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-07-13 10:24:59 +0100

    render exercises generated

--- src/components/Exercise.js
+++ src/components/Exercise.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import { OptionsContext } from "../App";
+
+import generateExercises from "../utils/generateExercise";
+import { DisplayExercise } from "./DisplayExercise";
 
 export const Exercise = () => {
+  const drillOptions = useContext(OptionsContext)
+  const exercises = generateExercises(drillOptions)
+
   return(
     <section className="h-full w-4/5 md:max-w-4xl flex flex-col justify-center">
-      <div className="flex justify-center">
-        <p>tu</p>
-        <input type="text"/>
-      </div>
+      <DisplayExercise exercises={exercises}/>
     </section>
   )
 }

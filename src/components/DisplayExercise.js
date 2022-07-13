16fa41e0f19b771ad9d21d35a754b8d33aeca3e2
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-07-13 11:47:35 +0100

    change styling and checkAnswer and provide error message

--- src/components/DisplayExercise.js
+++ src/components/DisplayExercise.js
@@ -5,11 +5,17 @@ import checkAnswer from "../utils/checkAnswer";
 export const DisplayExercise = ({exercises}) => {
     const [value, setValue] = useState('')
     const [execIndex, setExecIndex] = useState(0)
+    const [pass, setPass] = useState(null)
 
     const handleKeyPress = (e) => {
         if(e.key === "Enter"){
-            execIndex < exercises.length - 1 ? setExecIndex(execIndex + 1) : setExecIndex(0)
-            setValue('')
+            if(!checkAnswer(exercises[execIndex].conjugatedVerb, value)) {
+                setPass(<span>Incorrect answer, try again</span>)
+            } else {
+                execIndex < exercises.length - 1 ? setExecIndex(execIndex + 1) : setExecIndex(0)
+                setValue('')
+            }
+
         }
       }
     
@@ -18,19 +24,23 @@ export const DisplayExercise = ({exercises}) => {
       }
 
     return(
-        <div className="flex justify-center">
-            <div className="grow flex flex-col justify-end">
-                <span className="pr-2 text-end">{exercises[execIndex].pronoun}</span>
+        <div className="grid grid-cols-3 grid-rows-3 text-center text-lg">
+            <div className="col-start-1 col-span-1 row-start-2 row-span-1 text-end pr-2">{exercises[execIndex].pronoun}</div>
+
+            <div className="col-start-2 col-span-1 row-start-1 row-span-1">
+                {exercises[execIndex].tense}
             </div>
-            <div className="grow-0 text-center">
-                <div>
-                    {exercises[execIndex].tense}
-                </div>
-                <input value={value} onChange={handleChange} type="text" onKeyDown={handleKeyPress}/>
+            <div className="col-start-2 col-span-1 row-start-2 row-span-1">
+                <input className="max-w-full" value={value} onChange={handleChange} type="text" onKeyDown={handleKeyPress}/>
             </div>
-            <div className="grow flex flex-col justify-end">
-                <span className="pl-2 text-start">({exercises[execIndex].verb})</span>
+            <div className="col-start-2 col-end-3 row-start-3 row-span-1">
+                { pass }
             </div>
+
+            <div className="col-start-3 col-span-1 row-start-2 row-span-1 text-start pl-2">
+                <span className="pl-2 text-start">({exercises[execIndex].verbToConjugate})</span>
+            </div>
+            { console.log(exercises[execIndex].conjugatedVerb)}
         </div>
     )
 }

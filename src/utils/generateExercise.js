136e03c36efca3672f2d6a940e691fa6e6a58f5e
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-07-13 08:22:47 +0100

    add return and comment out unused variables

--- src/utils/generateExercise.js
+++ src/utils/generateExercise.js
@@ -1,9 +1,9 @@
 import { conjugate } from "conjugation-fr"
 import allVerbs from "../verbs-list/allVerbs"
 
-const generateExercise = (selectedOptions) => {
+const generateExercises = (selectedOptions) => {
     const sTenses = selectedOptions.getSelectedTenses()
-    const sCategories= selectedOptions.getSelectedCategories()
+    // const sCategories= selectedOptions.getSelectedCategories()
     const sPronouns= selectedOptions.getSelectedPronouns()
     const sVerbs = selectedOptions.getSelectedVerbs()[0]
     const sNum = selectedOptions.getSelectedAmount()
@@ -33,7 +33,7 @@ const generateExercise = (selectedOptions) => {
 
     for (let i = 0; i < sNum; i++) {
         let ranTense = sTenses[Math.floor(Math.random() * sTenses.length)].toLowerCase()
-        let ranCategory = sCategories[Math.floor(Math.random() * sCategories.length)].toLowerCase()
+        // let ranCategory = sCategories[Math.floor(Math.random() * sCategories.length)].toLowerCase()
         let ranPronoun = sPronouns[Math.floor(Math.random() * sPronouns.length)].toLowerCase()
 
         let ranVerb = verbPool[Math.floor(Math.random() * verbPool.length)]
@@ -47,6 +47,8 @@ const generateExercise = (selectedOptions) => {
 
         generatedList.push(exercise)
     }
+
+    return generatedList
 }
 
-export default generateExercise
+export default generateExercises

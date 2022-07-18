1b04aa2205fb1222847c470f66ab3902f0cc35b1
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-07-18 15:11:36 +0100

    Revert "create Footer component"
    This reverts commit dbbc720816a4f396d0f1cf5eb1856f70ae59e2f7.

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Footer = ({children}) => {
-    return(
-        <div className="fixed top-0 bottom-0 left-0 right-0 h-52">
-            {children}
-        </div>
-    )
-}

2c18096fe1dee74316441dd489581f747053ad11
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-07-19 13:25:29 +0100

    display default for buttons

--- src/components/BtnContent.js
+++ src/components/BtnContent.js
@@ -1,21 +1,17 @@
 import React from "react";
 import { observer } from "mobx-react-lite";
 
-export const BtnContent = observer(({ checks }) => {
+export const BtnContent = observer(({ checks, type }) => {
     let selected = ""
+    const placeholderText = `Select ${type.toLowerCase()}`
     Object.keys(checks).forEach(item => {
         if (checks[item]) {
             selected = selected + item + " "
         }
     })
-    if (selected) {
-        return(
-            <p className="max-w-full text-left overflow-hidden whitespace-nowrap text-ellipsis text-black/40">
-                {selected}
-            </p>
-        )
-    } else {
-        return null
-    }
-
+    return(
+        <p className="max-w-full text-left overflow-hidden whitespace-nowrap text-ellipsis text-black/40">
+            { selected || placeholderText}
+        </p>
+    )
 })

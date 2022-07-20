cdaeffeaeb31872511abd25aa3bf8180b25cb952
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-07-20 10:26:36 +0100

    make dropdown collapse when clicking outside

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,9 +1,9 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect} from "react";
 import { observer } from "mobx-react-lite"
 
 import { Checkbox } from "./";
 
-export const Dropdown = observer(({ type, options, checks, onClick}) => {
+export const Dropdown = observer(({ type, options, checks, handleClickOutside}) => {
     const [isDesktop, setIsDesktop] = useState(window.innerWidth > 640)
 
     const items = options.map((item) => 
@@ -14,7 +14,9 @@ export const Dropdown = observer(({ type, options, checks, onClick}) => {
         updatePredicate()
         window.addEventListener("resize", updatePredicate)
 
-        return () => window.removeEventListener("resize", updatePredicate)
+        return () => {
+            window.removeEventListener("resize", updatePredicate)
+        }
     })
 
     const updatePredicate = () => {
@@ -30,7 +32,7 @@ export const Dropdown = observer(({ type, options, checks, onClick}) => {
             ) 
         } else {
             return(
-                <div className="fixed inset-0 bg-black bg-opacity-50" onClick={onClick}>
+                <div className="fixed inset-0 bg-black bg-opacity-50" onClick={handleClickOutside}>
                     <div className="fixed h-52 inset-x-0 bottom-0 rounded-t-md bg-[#EDDDD4] overflow-scroll" onClick={(e) => {e.stopPropagation();}}>
                         {items}
                     </div>
--- src/components/PreferencesBtn.js
+++ src/components/PreferencesBtn.js
@@ -1,24 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { observer } from "mobx-react-lite";
 
 import { BtnContent, Dropdown } from ".";
 
 export const PreferencesBtn = observer(({ type, checks, options }) => {
-    const [show, setShow] = useState(false);
+    const menuRef = useRef(null);
+    const [listening, setListening] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
     const title = type[0] + type.slice(1).toLowerCase()
 
+    useEffect(() => {
+        if (listening) return;
+        if (!menuRef.current) return;
+        setListening(true);
+        [`click`, `touchstart`].forEach((type) => {
+          document.addEventListener(`click`, (evt) => {
+            if (menuRef.current.contains(evt.target)) return;
+            setIsOpen(false);
+          });
+        });
+      }, [ listening ])
+
     const handleClick = () => {
-        setShow(!show)
+        setIsOpen(!isOpen)
     }
 
     return(
-        <div className="sm:relative first:mt-4 my-4">
+        <div className="sm:relative first:mt-4 my-4" ref={menuRef}>
             <button className="h-20 w-full ring-2 ring-[#197278]/30 rounded-md px-4 py-2 flex flex-col justify-around" onClick={handleClick}>
                 <h3 className="w-full text-left text-[#197278] text-lg font-bold">{title}</h3>
                 <BtnContent checks={checks} type={type}/>
             </button>
-            { show && <Dropdown options={options} show={show} onClick={handleClick} checks={checks} type={type}/>}
+            { isOpen && <Dropdown options={options} handleClickOutside={handleClick} checks={checks} type={type}/>}
         </div>
     )
 })

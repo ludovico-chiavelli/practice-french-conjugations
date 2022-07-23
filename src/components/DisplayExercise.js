0e56a1294136242788f8aa46542d2bd523890ff3
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-07-23 10:44:00 +0100

    uplift state and animate displaying of exercise

--- src/components/DisplayExercise.js
+++ src/components/DisplayExercise.js
@@ -1,67 +1,37 @@
-import React, { useContext, useState } from "react";
-import { Redirect } from "react-router-dom";
-
-import checkAnswer from "../utils/checkAnswer";
-
-import { StatsContext } from "../App";
+import React, { useState, useEffect } from "react";
 import { observer } from "mobx-react-lite";
+import { animated, useSpring } from "react-spring";
 
-export const DisplayExercise = observer(({exercises}) => {
-    const stats = useContext(StatsContext)
+export const DisplayExercise = observer(({exercise, pass, handleKeyPress}) => {
+    const style = useSpring({ opacity: 1, from: { opacity: 0 } })
+
     const [value, setValue] = useState('')
-    const [execIndex, setExecIndex] = useState(0)
-    const [pass, setPass] = useState(null)
-    const [attempt, setAttempt] = useState(1)
-    const [completed, setCompleted] = useState(false)
-    
-    const handleKeyPress = (e) => {
-        if(e.key === "Enter" && attempt === 1){
-            if(!checkAnswer(exercises[execIndex].conjugatedVerb, value)) {
-                setPass(exercises[execIndex].conjugatedVerb)
-                setAttempt(2)
-            } else {
-                handleContinue()
-            }
 
-        } else if (e.key === "Enter" && attempt === 2) {
-            setAttempt(1)
-            handleContinue()
-        }
-      }
-    
     const handleChange = (e) => {
         setValue(e.target.value)
     }
 
-    const handleCompletion = () => {
-        setCompleted(true)
-    }
-
-    const handleContinue = () => {
-        stats.setResponse(execIndex, value, checkAnswer(exercises[execIndex].conjugatedVerb, value))
-        setPass(null)
+    useEffect(() => {
         setValue('')
-        execIndex < exercises.length - 1 ? setExecIndex(execIndex + 1) : handleCompletion()
-    }
-
+    }, [exercise])
 
     return(
-        <div className="space-y-2 text-xl sm:text-4xl">
-            <div className="text-center">
-                {exercises[execIndex].tense}
-            </div>
+        <animated.div style={style}>
+            <div className="space-y-2 text-xl sm:text-4xl">
+                <div className="text-center">
+                    {exercise.tense}
+                </div>
 
-            <div className="flex">
-                <span className="grow text-right pr-2">{exercises[execIndex].pronoun}</span>
-                <input className="grow-0 px-2" style={{width: (exercises[execIndex].verbToConjugate.length + 4).toString() + "ch"}} value={value} onChange={handleChange} type="text" onKeyDown={handleKeyPress}/>
-                <span className="grow pl-2 text-start">({exercises[execIndex].verbToConjugate})</span>
-            </div>
+                <div className="flex">
+                    <span className="grow text-right pr-2">{exercise.pronoun}</span>
+                    <input className="grow-0 px-2" style={{width: (exercise.verbToConjugate.length + 4).toString() + "ch"}} value={value} onChange={handleChange} type="text" onKeyDown={handleKeyPress}/>
+                    <span className="grow pl-2 text-start">({exercise.verbToConjugate})</span>
+                </div>
 
-            <div className="text-center">
-                { pass }
+                <div className="text-center">
+                    { pass }
+                </div>
             </div>
-
-            { completed && <Redirect to="/results"/>}
-        </div>
+        </animated.div>
     )
 })
--- src/components/ExerciseDrill.js
+++ src/components/ExerciseDrill.js
@@ -1,22 +1,57 @@
-import React, { useContext, useEffect } from "react";
+import React, { useState, useContext, useEffect } from "react";
+import { Redirect } from "react-router-dom";
 
 import { OptionsContext, StatsContext } from "../App";
 
 import generateExercises from "../utils/generateExercise";
+import checkAnswer from "../utils/checkAnswer";
 import { DisplayExercise } from "./DisplayExercise";
 
 export const ExerciseDrill = () => {
   const drillOptions = useContext(OptionsContext)
   const stats = useContext(StatsContext)
   const exercises = generateExercises(drillOptions)
+
+  const [execIndex, setExecIndex] = useState(0)
+  const [attempt, setAttempt] = useState(1)
+  const [pass, setPass] = useState(null)
+  const [completed, setCompleted] = useState(false)
+    
   
   useEffect(() => {
     stats.initializeResponses(exercises)
   }, [ stats, exercises ])
+  
+  const handleKeyPress = (e) => {
+    const value = e.target.value
+    if(e.key === "Enter" && attempt === 1){
+      if(!checkAnswer(exercises[execIndex].conjugatedVerb, value)) {
+        setPass(exercises[execIndex].conjugatedVerb)
+        setAttempt(2)
+      } else {
+        handleContinue(value)
+      }
+      
+    } else if (e.key === "Enter" && attempt === 2) {
+      setAttempt(1)
+      handleContinue()
+    }
+  }
+
+  const handleContinue = (value) => {
+    stats.setResponse(execIndex, value, checkAnswer(exercises[execIndex].conjugatedVerb, value))
+    setPass(null)
+    execIndex < exercises.length - 1 ? setExecIndex(execIndex + 1) : handleCompletion()
+  }
+  
+  const handleCompletion = () => {
+    setCompleted(true)
+  }
 
   return(
     <section className="h-full w-4/5 md:max-w-4xl flex flex-col justify-center">
-      <DisplayExercise exercises={exercises}/>
+      <DisplayExercise exercise={exercises[execIndex]} execIndex={execIndex} pass={pass} handleKeyPress={handleKeyPress} key={execIndex}/>
+      { completed && <Redirect to="/results"/>}
     </section>
   )
 }

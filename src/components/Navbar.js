0c02f4e217858d7a1f873c0b5d0181d35d91cfc8
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-08-01 16:36:51 +0100

    add logo and BMC link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
+import bmcLogo from "../assets/bmc-logo.svg"
+
 export const Navbar = () => {
     return (
-        <header className='h-16 flex justify-center py-5 rounded-b-md drop-shadow-md shadow-md' style={{backgroundColor: '#197278'}}>
-            <p>🔥</p>
+        <header className='h-16 flex justify-between py-4 px-12 rounded-b-md drop-shadow-md shadow-md text-2xl' style={{backgroundColor: '#197278'}}>
+            <p className='font-yellowtail text-white'>🇫🇷 Practice French</p>
+            <a href="https://www.buymeacoffee.com/SailTrain" className='rounded-md ring-2 ring-[#ffdd00] h-8 w-8 flex justify-center items-center'>
+                <img className='h-6 w-6' src={bmcLogo} alt="Buy Me a Coffee"></img>
+            </a>
         </header>
     )
 }
